a2ba3a846ed874595a2461be96fc9db239739b43
fortune710 <fortune0208@yahoo.com>
2023-12-23 21:52:32 +0100

    added tailwind and supabase

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--dark-green)",
+        
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
