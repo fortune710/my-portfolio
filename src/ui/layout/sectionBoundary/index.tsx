c025727151d334b79ccb65f7d1fe8837f3765686
fortune710 <fortune0208@yahoo.com>
2024-05-02 02:17:49 -0400

    deleted stuff

--- src/ui/layout/sectionBoundary/index.tsx
+++
@@ -1,7 +0,0 @@
-export function SectionBoundary({ children }: { children: React.ReactNode }) {
-  return (
-    <section className="min-h-20 flex justify-center items-center">
-      {children}
-    </section>
-  );
-}
