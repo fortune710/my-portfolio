62085ecfd352f09e96068e3e2d75461867838349
fortune710 <fortune0208@yahoo.com>
2024-05-02 10:30:10 -0400

    404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,8 @@
-import { PageLayout } from "@/ui";
 
 export default function NotFoundPage() {
   return (
-    <PageLayout className="flex justify-center items-center">
+    <div className="flex justify-center items-center">
       <h1 className="font-semibold">Sorry but Page Not Found </h1>
-    </PageLayout>
+    </div>
   );
 }
