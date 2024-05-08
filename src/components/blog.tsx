f03f8b41d5b3daf31cb4ef20a7e71376611adeec
fortune710 <fortune0208@yahoo.com>
2024-05-08 13:07:08 -0400

    blog component

---
+++ src/components/blog.tsx
@@ -0,0 +1,33 @@
+import { getHashnodeArticles } from "@/utils/hashnode"
+import Image from "next/image";
+
+export default async function Blog() {
+    const articles = await getHashnodeArticles();
+
+    return (
+        <div className="flex items-center pl-6 gap-4">
+            {
+                articles.splice(0, 3).map((article) => (
+                    <div className="text-center max-w-[300px]" key={article.id}>
+                        <Image
+                            src={article.coverImage.url}
+                            alt={article.title}
+                            width={275}
+                            height={188}
+                            className="rounded-[32px] object-cover w-[275px] h-[188px]"
+                        />
+
+                        <div className="mt-5">
+                            <h1 className="uppercase text-[#757575] font-bold font-inter text-sm">
+                                {article.title}
+                            </h1>
+                            <p className="font-semibold text-xl">
+                                {article.subtitle}
+                            </p>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
