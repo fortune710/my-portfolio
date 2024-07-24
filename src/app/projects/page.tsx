f8741af632f53338416664514cb4e97080b0d721
fortune710 <fortune0208@yahoo.com>
2024-07-24 15:47:22 -0400

    created new component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,5 @@
+import { ProjectCardNew } from "@/components/project-card"
 import { getProjects } from "@/services/projects"
-import Link from "next/link"
-import Image from "next/image"
 
 
 export default async function ProjectsPage() {
@@ -13,41 +12,10 @@ export default async function ProjectsPage() {
                 <p className="font-medium font-jarkata text-lg lg:text-2xl">Check out the cool stuff I&apos;ve built!</p>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 px-3 md:px-10 lg:px-16 gap-5 pt-3 md:pt-4">
+            <div className="animate-fade-in-bottom grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-3 md:px-10 lg:px-16 gap-5 pt-3 md:pt-4 gap-y-5 md:gap-y-7">
                 {
-                projects.map((project) => (
-                    <div className="w-full bg-dark border border-dark-muted rounded-[40px] px-8 py-10 space-y-3 text-white" key={project.title}>
-                    <div className="flex justify-between items-start">
-                        <Image
-                        src={project.logo}
-                        alt={project.title}
-                        width={200}
-                        height={200}
-                        className="rounded-lg max-sm:w-[100px] max-sm:h-[100px]"
-                        />
-
-                        <Link target="_blank"  className="border border-white rounded-md py-4 px-5 text-white font-medium" href={project.link}>
-                        Checkout Live App
-                        </Link>
-                    </div>
-
-                    <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl">{project.title}</h2>
-                    <p>{project.year} - {project.type}</p>
-
-                    <p className="lg:text-lg text-[#B3B3B3]">{project.description}</p>
-
-                    <Image
-                        src={project.image}
-                        alt={project.title}
-                        width={500}
-                        height={500}
-                        className="rounded-lg w-full"
-                    />
-
-                    </div>
-                ))
+                    projects.map((project) => <ProjectCardNew project={project}/>)
                 }
-
             </div>
         </main>
     )
---
+++ src/components/project-card.tsx
@@ -0,0 +1,63 @@
+import { Project } from "@/services/projects";
+import Link from "next/link"
+import Image from "next/image"
+import { MoveUpRight } from "lucide-react";
+
+
+export const ProjectCard = ({ project }: { project: Project }) => (
+    <div className="w-full bg-dark border border-dark-muted rounded-[40px] px-8 py-10 space-y-3 text-white" key={project.title}>
+        <div className="flex justify-between items-start">
+            <Image
+            src={project.logo}
+            alt={project.title}
+            width={200}
+            height={200}
+            className="rounded-lg max-sm:w-[100px] max-sm:h-[100px]"
+            />
+
+            <Link target="_blank"  className="border border-white rounded-md py-4 px-5 text-white font-medium" href={project.link}>
+                Checkout Live App
+            </Link>
+        </div>
+
+        <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl">{project.title}</h2>
+        <p>{project.year} - {project.type}</p>
+
+        <p className="lg:text-lg text-[#B3B3B3]">{project.description}</p>
+
+        <Image
+            src={project.image}
+            alt={project.title}
+            width={500}
+            height={500}
+            className="rounded-lg w-full"
+        />
+
+    </div>
+)
+
+export const ProjectCardNew = ({ project }: { project: Project }) => {
+
+    return (
+        <div>
+            <Image
+                src={project.image}
+                alt={project.title}
+                width={500}
+                height={500}
+                className="rounded-lg w-full"
+            />
+
+            <div className="flex justify-between items-center mt-5">
+                <div>
+                    <h2 className="font-semibold text-2xl">{project.title}</h2>
+                    <p>{project.year} - {project.type}</p>
+                </div>
+
+                <button>
+                    <MoveUpRight/>
+                </button>
+            </div>
+        </div>
+    )
+}
